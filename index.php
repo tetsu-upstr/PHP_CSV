6b865cad75406ef0e0a8d50e3b8137943424e277
tetsu-upstr <bodomu@gmail.com>
2020-03-29 08:52:24 +0900

    集計SQL追記

--- index.php
+++ index.php
@@ -15,28 +15,56 @@ require 'connect.php';
 
 <?php
 
-$sql = "SELECT * FROM item ";
+// 商品リスト
+// $sql = "SELECT * FROM item ";
+// $result = $pdo->prepare($sql);
+// $result->execute();
 
-$result = $pdo->prepare($sql);
-$result->execute();
+// 売上実績
+$sql = "SELECT item_name, DATE_FORMAT(month, '%Y年%m月') as sale_month, SUM(amount) as count  FROM sales_result GROUP BY DATE_FORMAT(month, '%Y年%m月')";
+$sales_result = $pdo->prepare($sql);
+$sales_result->execute();
 
 ?>
+
+<button class="btn"><a href="input.php">商品登録</a></button>
 
 <table class="item-table">
   <tr>
     <th>品名</th>
+    <th>販売月</th>
+    <th>数量</th>
+  
+    <?php
+    foreach($sales_result as $row) {
+      echo '<tr>';
+      echo '<td>' . $row['item_name'] .'</td>';
+      echo '<td>' . $row['sale_month'] .'</td>';
+      echo '<td>' . $row['count'] .'</td>';
+      echo '</tr>';
+    }
+    ?>
+
+</table>
+
+
+<!-- <table class="item-table">
+  <tr>
+    <th>品名</th>
     <th>JAN</th>
+    <th>規格</th>
   
     <?php
     foreach($result as $row) {
       echo '<tr>';
       echo '<td>' . $row['item_name'] .'</td>';
       echo '<td>' . $row['jan'] .'</td>';
+      echo '<td>' . $row['standard'] .'</td>';
       echo '</tr>';
     }
     ?>
 
-</table>
+</table> -->
 
 </body>
 </html>
