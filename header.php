c366b857f456fe0ac4f34fb74a625881f9ac1875
tetsu-upstr <bodomu@gmail.com>
2020-03-29 17:42:31 +0900

    検索機能

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php require 'connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+  <!-- cSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <title>Sales</title>
+</head>
+
+<header>
+  <p><i class="fas fa-tachometer-alt"></i> Sales dashboard</p>
+</header>
+
+
--- index.php
+++ index.php
@@ -1,60 +1,86 @@
-<?php 
-require 'connect.php';
+<?php require 'header.php'; ?>
 
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-  <link rel="stylesheet" href="css/style.css">
-  <title>Sales</title>
-</head>
 <body>
-
 <?php
 
-// 検索
+// 売上実績の検索
+if(isset($_POST['search'])) {
 
-$sql = "SELECT item_name, DATE_FORMAT(month, '%Y年%m月') as sale_month, SUM(amount) as count FROM sales_result";
+  // 全件検索
+  if (empty($_POST['name']) && empty($_POST['store'])) {
+    $sql = "SELECT item_name, DATE_FORMAT(month, '%Y年%m月') as sale_month, SUM(amount) as count, unit_price, store FROM sales_result ";
+  }
 
-if($_POST['start'] == "" && $_POST['end'] == "") {
-  $sql .= " GROUP BY DATE_FORMAT(month, '%Y年%m月')";
-} else {
-  $start = $_POST['start'];
-  $end = $_POST['end'];
-  $sql .= " WHERE month BETWEEN '$start' AND '$end' GROUP BY DATE_FORMAT(month, '%Y年%m月')";
+  // 品名と店舗の検索
+  if (!empty($_POST['name']) && !empty($_POST['store'])) {
+    $name = $_POST['name'];
+    $store = $_POST['store'];
+    $sql = "SELECT item_name, DATE_FORMAT(month, '%Y年%m月') as sale_month, SUM(amount) as count, unit_price, store FROM sales_result WHERE item_name LIKE '%{$name}%' AND store LIKE '%{$store}%' ";
+    // var_dump($sql);
+  }
+    
+  // 品名検索
+  if (!empty($_POST['name']) && empty($_POST['store'])) {
+    $name = $_POST['name'];
+    $sql = "SELECT item_name, DATE_FORMAT(month, '%Y年%m月') as sale_month, SUM(amount) as count, unit_price, store FROM sales_result WHERE item_name LIKE '%{$name}%' ";
+  }
+
+  // 店舗検索
+  if (!empty($_POST['store']) && empty($_POST['name'])){
+    $store = $_POST['store'];
+    $sql = "SELECT item_name, DATE_FORMAT(month, '%Y年%m月') as sale_month, SUM(amount) as count, unit_price, store FROM sales_result WHERE store LIKE '%{$store}%' ";
+  }
+
+  // SQL共通の整列条件
+  $sql .= " GROUP BY item_name, store, DATE_FORMAT(month, '%Y年%m月') ORDER BY month";
+
+  $result = $pdo->prepare($sql);
+  $result->execute();
 }
 
-$sales_result = $pdo->prepare($sql);
-$sales_result->execute();
+// 集計期間の指定
+if(isset($_POST['period_search'])) {
+
+  $sql = "SELECT item_name, DATE_FORMAT(month, '%Y年%m月') as sale_month, SUM(amount) as count, unit_price, store FROM sales_result";
+
+  if($_POST['start'] == "" && $_POST['end'] == "") {
+    $sql .= " GROUP BY item_name, store, DATE_FORMAT(month, '%Y年%m月') ORDER BY month";
+  } else {
+    $start = $_POST['start'];
+    $end = $_POST['end'];
+    $sql .= " WHERE month BETWEEN '$start' AND '$end' GROUP BY DATE_FORMAT(month, '%Y年%m月') ORDER BY month";
+  }
+
+  $result = $pdo->prepare($sql);
+  $result->execute();
+
+}
 
 ?>
 
-<div>
-  <table>
-    <tr><td>商品検索</td>
+  <table class="function-table">
+    <tr>
+      <th class="th-2">検索</th>
+      <th class="th-2">集計期間</th>
+      <th class="th-3"></th>
+    </tr>
     <form action="index.php" method="POST">
-      <td><input type="text" name="name"></input></td>
-      <td><input type="submit" name="search" class="btn"></td></tr>  
+      <td>
+        <input type="text" name="name" placeholder="品名">
+        <input type="text" name="store" placeholder="店名">
+        <input type="submit" name="search" class="btn" value="検索">
+      </td>
+    </form>
+    
+    <form action="index.php" method="POST">
+      <td><input type="date" name="start">〜<input type="date" name="end">
+      <input type="submit" name="period_search" class="btn" value="検索"></td>
+      <td><button class="btn"><a href="#">商品登録</a></button></td> </tr> 
     </form>
   </table>
-  
-</div>
 
-<div>
-  <p>集計期間</p>
-  <form action="index.php" method="POST">
-    開始<input type="date" name="start">
-    終了<input type="date" name="end">
-       <input type="submit" name="check" class="btn">
-  </form>
-</div>
 
-<button class="btn"><a href="input.php">商品登録</a></button>
+
 
 <p><i class="fas fa-money-check"></i> 販売実績</p>
 <table class="item-table">
@@ -62,13 +88,19 @@ $sales_result->execute();
     <th>品名</th>
     <th>販売月</th>
     <th>数量</th>
+    <th>単価</th>
+    <th>売上金額</th>
+    <th>店舗</th>
   
     <?php
-    foreach($sales_result as $row) {
+    foreach($result as $row) {
       echo '<tr>';
       echo '<td>' . $row['item_name'] .'</td>';
       echo '<td>' . $row['sale_month'] .'</td>';
       echo '<td>' . $row['count'] .'</td>';
+      echo '<td>' . $row['unit_price'] .'</td>';
+      echo '<td>' . number_format($row['unit_price'] * $row['count']) .'</td>';
+      echo '<td>' . $row['store'] .'</td>';
       echo '</tr>';
     }
     ?>
@@ -77,9 +109,8 @@ $sales_result->execute();
 
 <?php
 
-
 // 商品リスト
-$sql = "SELECT * FROM item ";
+$sql = "SELECT * FROM item";
 $result = $pdo->prepare($sql);
 $result->execute();
 
@@ -88,6 +119,7 @@ $result->execute();
 <p><i class="fas fa-save"></i> 商品リスト</p>
 <table class="item-table">
   <tr>
+    <th>カテゴリ</th>
     <th>品名</th>
     <th>JAN</th>
     <th>規格</th>
@@ -99,6 +131,7 @@ $result->execute();
     <?php
     foreach($result as $row) {
       echo '<tr>';
+      echo '<td>' . $row['category'] .'</td>';
       echo '<td>' . $row['item_name'] .'</td>';
       echo '<td>' . $row['jan'] .'</td>';
       echo '<td>' . $row['standard'] .'</td>';
