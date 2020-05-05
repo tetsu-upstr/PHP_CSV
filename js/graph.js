c01042697edb33e49abf7cdb848e48b3f44da9e2
tetsu-upstr <bodomu@gmail.com>
2020-05-06 06:46:18 +0900

    option:yAxes追記

--- js/graph.js
+++ js/graph.js
@@ -1,40 +1,57 @@
-// < Chart.js MITライセンス >
-// Copyright (c) 2018 Chart.js Contributors
-// Released under the MIT license
-// https://opensource.org/licenses/MIT
+export function drawChart() {
 
-$(function(){
+  var label_month = [];
+  var sales_data = [];
 
   // labelsの値を取得
   $('.js-sales_month').each(function(){
     var amount01 = $(this).find('span').text();
-    array01.push(amount01);
+    label_month.push(amount01);
   })
   // dataの値を取得
   $('.js-proceeds').each(function(){
     var amount02 = $(this).find('span').text();
-    array02.push(amount02);
+    sales_data.push(amount02);
   })
 
   var ctx = document.getElementById('myChart').getContext('2d');
   var chart = new Chart(ctx, {
-    // 作成したいチャートのタイプ
+
+    // 作成したいチャートタイプ
     type: 'bar',
 
-    // データセットのデータ
+    // グラフのデータ
     data: {
         // labels: ['1月', '2月', '3月'],
-        labels: array01,
+        labels: label_month,
+
         datasets: [{
             label: "販売実績の推移",
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
-            data: array02
+            data: sales_data
         }]
     },
 
-    // ここに設定オプションを書きます
-    options: { responsive: false }
+    // 設定オプション
+    options: { 
+      responsive: false,
+      scales: {
+        // 縦軸の設定
+        yAxes: [{
+          ticks: {
+            suggestedMin: 0,
+            suggestedMax: 10000,
+            stepSize: 1000
+          }
+        }]
+      }
+     }
   });
 
-});
+  // < Chart.js MITライセンス >
+  // Copyright (c) 2018 Chart.js Contributors
+  // Released under the MIT license
+  // https://opensource.org/licenses/MIT
+  
+}
